062d93fa3421471745a20b0c614e34f97326b3c1
Tashby <tashby2714@gmail.com>
2017-06-09 02:45:10 -0400

    current user tasks controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170609054231) do
+ActiveRecord::Schema.define(version: 20170609063058) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.string   "state",      default: "open"
+    t.integer  "user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
