b5b1607faa8af9994f19652c283eb1c1e47bfbe3
Tashby <tashby2714@gmail.com>
2017-06-09 03:03:23 -0400

    ability to change state and have views reflect the change

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,15 +1,13 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :set_task, only: [:show, :edit, :update, :destroy, :change]
 
   # GET /tasks
   # GET /tasks.json
   def index
-    @tasks = current_user.tasks
     @open = current_user.tasks.where(state: "open")
      @wip = current_user.tasks.where(state: "wip")
      @closed = current_user.tasks.where(state: "closed")
-
   end
 
   # GET /tasks/1
@@ -64,6 +62,13 @@ class TasksController < ApplicationController
       format.html { redirect_to tasks_url, notice: 'Task was successfully destroyed.' }
       format.json { head :no_content }
     end
+  end
+
+  def change
+    @task.update_attributes(state: params[:state])
+      respond_to do |format|
+        format.html { redirect_to tasks_path, notice: "Task updated"}
+      end
   end
 
   private
